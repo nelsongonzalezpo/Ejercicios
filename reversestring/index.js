8e62e2d7eb31adc3fce2d7be8e4b7e5fbaa7ce59
nelsongonzalezpo <nelson.gonzalezpo@udlap.mx>
2018-06-13 16:08:08 -0500

    Actualizacion Reverse String

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,14 +6,65 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str)
-{
 
-nuevo = str.split("");
-return nuevo.reverse().join("");
+//Primera solucion
+
+
+// function reverse(str)
+// {
+//
+//   const arreglo = str.split('');
+//   arreglo.reverse();
+//   return arreglo.join('');
+//
+// }
+
+//Segunda solucion
+
+//Recomendacion, hacerlo con la segunda funcion
+
+function reverse(str){
+
+  let reversed = '';
+
+  for(let letra of str){
+    reversed = letra + reversed;
+  }
+
+   return reversed;
 
 }
-module.exports = reverse;
+
+//Tercera Solucion
+
+// function reverse(str){
+//
+// reduce single string value
+//  Esta funcion esta hecha con funciones de flecha
+//
+// return str.split('').reduce((reversed, character) => {
+//  return character + reversed;
+// }, '');
+//
+//
+// }
+
+
+
+ module.exports = reverse;
+
+
+
+
+
+// var palabra = str.split('');
+// for(int i = 0; i = palabra.length(); i ++){
+//
+//   var nuevaPalabra += palabra;
+//
+// }
+//
+// return nuevaPalabra.reverse();
 
 
 
