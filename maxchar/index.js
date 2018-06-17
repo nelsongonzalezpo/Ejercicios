29c75404d260ed7918a68bc80708ff509ad93952
nelsongonzalezpo <nelson.gonzalezpo@udlap.mx>
2018-06-17 02:03:25 -0500

    Adding MaxChar

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,14 +5,40 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+//counting numbers of charac
 function maxChar(str) {
-  contador = 0;
-  palabra = str.split('');
+  //palabra = str.split('');
+  charMap = {};
+  max = 0;
 
-  for (let char of string)
+
+//normal loop
+  for (let char of str)
   {
-     
+    //Si ya existe, se incrementa
+    if(charMap[char]){
+      charMap[char] ++;
+    }
+
+    else{
+      //en caso de que no exista, es 1 maximo
+      charMap[char] = 1;
+    }
   }
+
+//loop to iterate objects
+  for (let char in charMap){
+
+    if (charMap[char] > max){
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  //Ya tenemos los valores
+  console.log(charMap);
+  console.log(maxChar)
+  return maxChar
 
 }
 
