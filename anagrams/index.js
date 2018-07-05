71e607a4e8f5976a11cdb85d182abce3d702e45d
nelsongonzalezpo <nelson.gonzalezpo@udlap.mx>
2018-07-05 13:12:01 -0500

    Adding Anagram

--- anagrams/index.js
+++ anagrams/index.js
@@ -18,9 +18,17 @@ function anagrams(stringA, stringB) {
   const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
 
-  
+  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+    return false;
+  }
 
+  for(let char in aCharMap){
+    if(aCharMap[char] !== bCharMap[char]){
+      return false;
+    }
+  }
 
+  return true;
 
 }
 
@@ -29,7 +37,7 @@ function buildCharMap(str){
 
   for(let char of str.replace(/[^\w]/g, "").toLowerCase()){
 
-    charMap[char] = charMap[char] ++ || 1;
+    charMap[char] = charMap[char] +  1 || 1;
   }
 
   return charMap;
